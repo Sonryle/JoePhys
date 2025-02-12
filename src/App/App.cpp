9fa232dd2c208a3bb9fa04710b31c245f210785c
Joseph <cathcartj08@gmail.com>
2025-02-12 18:27:55 +0200

    Add GLFW and clean up build system
    Added the source code to GLFW. It will be compiled WITH our application
    rather than me just linking a glfw3.lib or libglfw3.a file. This is just
    for a cleaner build system (we dont have to have separate libraries for
    both windows and linux now, and the build config of GLFW will now always match
    the build config of our application, which is neat.

--- src/App/App.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "Core/Core.hpp"
-
-int main()
-{
-    std::cout << "Core Version:\t" << CORE_VERSION << "\n\n";
-    Core::SayHello();
-    return 0;
-}
---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <GLFW/glfw3.h>
+
+int main()
+{
+    glfwInit();
+    std::cout << "Hi he mian whatchamacallit idk im so sorr yfuture joseph\n";
+    std::cin.get();
+    return 0;
+}
