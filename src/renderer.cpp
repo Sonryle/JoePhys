1e7ee620d962931d67d23b35e99a12ba25a66ac1
Joseph <cathcartj08@gmail.com>
2025-02-13 16:06:23 +0200

    Update Renderer
    The renderer now renders a triangle when renderer.render() is called
    (you must first call renderer.init(). This is just temporary and the
    next step will be for me to implement a shader class for easier shader
    creation and management.

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -1,8 +1,93 @@
 #include "renderer.hpp"
 
+unsigned int vertex_shader;
+unsigned int fragment_shader;
+unsigned int shader_program;
+unsigned int VAO;
+unsigned int VBO;
+
+const char* vertex_shader_source = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
+const char* fragment_shader_source = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"   FragColor = vec4(0.6f, 0.72f, 0.6f, 1.0f);\n"
+"}\n\0";
+
+float vertices[] = {
+-0.5f, -0.5f, 0.0f,
+ 0.5f, -0.5f, 0.0f,
+ 0.0f,  0.5f, 0.0f
+};
+
+
 void Renderer::init()
 {
+	std::cout << "initialising renderer!\n";
 
+	// Set up shaders
+
+	vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertex_shader, 1, &vertex_shader_source, NULL);
+	glCompileShader(vertex_shader);
+	int  success;
+	char infoLog[512];
+	glGetShaderiv(vertex_shader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(vertex_shader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+	else
+		std::cout << "vertex shader successfully compiled!\n";
+
+	fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragment_shader, 1, &fragment_shader_source, NULL);
+	glCompileShader(fragment_shader);
+	glGetShaderiv(fragment_shader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(fragment_shader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+	else
+		std::cout << "fragment shader successfully compiled!\n";
+
+	shader_program = glCreateProgram();
+	glAttachShader(shader_program, vertex_shader);
+	glAttachShader(shader_program, fragment_shader);
+	glLinkProgram(shader_program);
+	glGetProgramiv(shader_program, GL_LINK_STATUS, &success);
+	if (!success) {
+		glGetProgramInfoLog(shader_program, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER_PROGRAM::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+	else
+		std::cout << "shader program successfully created!\n";
+	glUseProgram(shader_program);
+	glDeleteShader(vertex_shader);
+	glDeleteShader(fragment_shader);
+
+	// Set up VAO
+
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
+
+	// Set up VBO and attach buffer data
+
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	// Set vertex attributes
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
 
 	return;
 }
@@ -10,8 +95,14 @@ void Renderer::init()
 void Renderer::render()
 {
 	// set background to be red
-	glClearColor(0.95f, 0.51f, 0.49f, 1.0f);
+	glClearColor(0.92f, 0.28f, 0.37f, 1.0f);
 	glClear(GL_COLOR_BUFFER_BIT);
+
+
+	std::cout << "rendering\n";
+	glUseProgram(shader_program);
+	glBindVertexArray(VAO);
+	glDrawArrays(GL_TRIANGLES, 0, 3);
 
 	return;
 }
