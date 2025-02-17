e622f360fb05296fce9c70fffa6ec8e587528d4e
Joseph <cathcartj08@gmail.com>
2025-02-17 19:16:07 +0200

    Remove temporary Code
    Removed all temporary code from main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -62,72 +62,6 @@ int main()
 	// Initialise renderer
 	renderer.init();
 
-	// TEMPORARY SHAPES
-	Circle tempCirc;
-	tempCirc.layer = 1;
-	tempCirc.position = glm::vec2(0.0f, 0.0f);
-	tempCirc.radius = 50;
-	tempCirc.colour = glm::vec4(0.3f, 1.0f, 0.3f, 1.0f);
-
-	Circle tempCircTwo;
-	tempCircTwo.layer = 1;
-	tempCircTwo.position = glm::vec2(0.0f, 0.0f);
-	tempCircTwo.radius = 50;
-	tempCircTwo.colour = glm::vec4(0.3f, 1.0f, 0.3f, 1.0f);
-
-	Line tempLine;
-	tempLine.layer = 0;
-	tempLine.thickness = 40;
-	tempLine.start_position = glm::vec2(100.0f, -200.0f);
-	tempLine.end_position = glm::vec2(-100.0f, 200.0f);
-	tempLine.colour = glm::vec4(0.3f, 0.3f, 1.0f, 1.0f);
-
-	Square tempSquare;
-	tempSquare.layer = -1;
-	tempSquare.x_scale = 200;
-	tempSquare.y_scale = 200;
-	tempSquare.position = glm::vec2((window.width / 2.0f) - (tempSquare.x_scale / 2.0f), (window.height / 2.0f) - (tempSquare.y_scale / 2));
-	tempSquare.top_left_tex_coord = glm::vec2(0.0f, 1.0f);
-	tempSquare.top_right_tex_coord = glm::vec2(0.25f, 1.0f);
-	tempSquare.bottom_left_tex_coord = glm::vec2(0.0f, 0.0f);
-	tempSquare.bottom_right_tex_coord = glm::vec2(0.25f, 0.0f);
-	tempSquare.colour = glm::vec4(0.4f, 0.8f, 0.1f, 1.0f);
-
-	Square tempSquare2;
-	tempSquare2.layer = -1;
-	tempSquare2.x_scale = 200;
-	tempSquare2.y_scale = 200;
-	tempSquare2.position = glm::vec2((window.width / -2.0f) + (tempSquare.x_scale / 2.0f), (window.height / 2.0f) - (tempSquare.y_scale / 2));
-	tempSquare2.top_left_tex_coord = glm::vec2(0.25f, 1.0f);
-	tempSquare2.top_right_tex_coord = glm::vec2(0.5f, 1.0f);
-	tempSquare2.bottom_left_tex_coord = glm::vec2(0.25f, 0.0f);
-	tempSquare2.bottom_right_tex_coord = glm::vec2(0.5f, 0.0f);
-	tempSquare2.colour = glm::vec4(0.4f, 0.8f, 0.1f, 1.0f);
-
-	Square tempSquare3;
-	tempSquare3.layer = -1;
-	tempSquare3.x_scale = 200;
-	tempSquare3.y_scale = 200;
-	tempSquare3.position = glm::vec2((window.width / 2.0f) - (tempSquare.x_scale / 2.0f), (window.height / -2.0f) + (tempSquare.y_scale / 2));
-	tempSquare3.top_left_tex_coord = glm::vec2(0.5f, 1.0f);
-	tempSquare3.top_right_tex_coord = glm::vec2(0.75f, 1.0f);
-	tempSquare3.bottom_left_tex_coord = glm::vec2(0.5f, 0.0f);
-	tempSquare3.bottom_right_tex_coord = glm::vec2(0.75f, 0.0f);
-	tempSquare3.colour = glm::vec4(0.4f, 0.8f, 0.1f, 1.0f);
-
-	Square tempSquare4;
-	tempSquare4.layer = -1;
-	tempSquare4.x_scale = 200;
-	tempSquare4.y_scale = 200;
-	tempSquare4.position = glm::vec2((window.width / -2.0f) + (tempSquare.x_scale / 2.0f), (window.height / -2.0f) + (tempSquare.y_scale / 2));
-	tempSquare4.top_left_tex_coord = glm::vec2(0.75f, 1.0f);
-	tempSquare4.top_right_tex_coord = glm::vec2(1.0f, 1.0f);
-	tempSquare4.bottom_left_tex_coord = glm::vec2(0.75f, 0.0f);
-	tempSquare4.bottom_right_tex_coord = glm::vec2(1.0f, 0.0f);
-	tempSquare4.colour = glm::vec4(0.4f, 0.8f, 0.1f, 1.0f);
-
-
-
 	// game loop
 	while (!glfwWindowShouldClose(window.handle))
 	{
@@ -138,13 +72,8 @@ int main()
 
 		// Update our clock
 		// ----------------
-		jp_clock.update();
 
-		// TEMPORARY FOR MYLES
-		tempSquare.position = glm::vec2((window.width / 2.0f) - (tempSquare.x_scale / 2.0f), (window.height / 2.0f) - (tempSquare.y_scale / 2));
-		tempSquare2.position = glm::vec2((window.width / -2.0f) + (tempSquare.x_scale / 2.0f), (window.height / 2.0f) - (tempSquare.y_scale / 2));
-		tempSquare3.position = glm::vec2((window.width / 2.0f) - (tempSquare.x_scale / 2.0f), (window.height / -2.0f) + (tempSquare.y_scale / 2));
-		tempSquare4.position = glm::vec2((window.width / -2.0f) + (tempSquare.x_scale / 2.0f), (window.height / -2.0f) + (tempSquare.y_scale / 2));
+		jp_clock.update();
 
 		// render scene
 		// ------------
@@ -152,42 +81,6 @@ int main()
 		// set background to be red
 		glClearColor(0.92f, 0.28f, 0.37f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-		// render objects	(objects must be rendered from back to front)
-		renderer.renderSquare(&tempSquare);
-		renderer.renderSquare(&tempSquare2);
-		renderer.renderSquare(&tempSquare3);
-		renderer.renderSquare(&tempSquare4);
-		renderer.renderLine(&tempLine);
-		renderer.renderCircle(&tempCirc);	// circles must always be rendered last
-		renderer.renderCircle(&tempCircTwo);	// since they have some transparent fragments
-
-		// TEMPORARY LINE MOVEMENT CODE
-		// ----------------------------
-		
-		float speed = 240;
-		if (glfwGetKey(window.handle, GLFW_KEY_UP))
-			tempLine.end_position = glm::vec2(tempLine.end_position.x, tempLine.end_position.y + speed * jp_clock.deltaTime);
-		if (glfwGetKey(window.handle, GLFW_KEY_DOWN))
-			tempLine.end_position = glm::vec2(tempLine.end_position.x, tempLine.end_position.y - speed * jp_clock.deltaTime);
-
-		if (glfwGetKey(window.handle, GLFW_KEY_LEFT))
-			tempLine.end_position = glm::vec2(tempLine.end_position.x - speed * jp_clock.deltaTime, tempLine.end_position.y);
-		if (glfwGetKey(window.handle, GLFW_KEY_RIGHT))
-			tempLine.end_position = glm::vec2(tempLine.end_position.x + speed * jp_clock.deltaTime, tempLine.end_position.y);
-
-		if (glfwGetKey(window.handle, GLFW_KEY_W))
-			tempLine.start_position = glm::vec2(tempLine.start_position.x, tempLine.start_position.y + speed * jp_clock.deltaTime);
-		if (glfwGetKey(window.handle, GLFW_KEY_S))
-			tempLine.start_position = glm::vec2(tempLine.start_position.x, tempLine.start_position.y - speed * jp_clock.deltaTime);
-
-		if (glfwGetKey(window.handle, GLFW_KEY_A))
-			tempLine.start_position = glm::vec2(tempLine.start_position.x - speed * jp_clock.deltaTime, tempLine.start_position.y);
-		if (glfwGetKey(window.handle, GLFW_KEY_D))
-			tempLine.start_position = glm::vec2(tempLine.start_position.x + speed * jp_clock.deltaTime, tempLine.start_position.y);
-
-		tempCirc.position = tempLine.start_position;
-		tempCircTwo.position = tempLine.end_position;
 
 		// swap buffers and poll input events
 		// ----------------------------------
