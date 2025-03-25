a125414f4ca8954b2523e20e88a69c41bf4bfaa5
Joseph <cathcartj08@gmail.com>
2025-03-25 14:06:47 +0200

    Fixed Newton's Cradle Scene
    When using Newton's Cradle Scene with a high substep count, the springs
    would freak out and explode. This was because some of the particles
    were being spawned very far away from where they were meant to spawn,
    and so the springs would freak out and pull them extremely quickly back
    to where they were meant to be.

--- testbed/scenes/NewtonsCradleScene.hpp
+++ testbed/scenes/NewtonsCradleScene.hpp
@@ -18,34 +18,9 @@ struct NewtonsCradleScene : public Scene
 		SetUpSceneColours();
 		world->Create(settings.simulation_hertz, settings.sub_steps, vec2(0.0f, -9.8f));
 		settings.circle_res = 20;
+		camera.zoom = 2.0f;
 		double PI = 3.141592653589;
 
-		// Add a ball to hit the 4 balls
-		Cluster* cradle = new Cluster;
-
-		vec2 vel(0.0f, 0.0f);
-		real elas = 1.0f;
-		real rad = 0.75f;
-		vec2 pos((-1 * rad * 2) - rad * 2 - 6.0f, 4.0f);
-		real mass = PI * rad * rad;
-		Particle* p = new Particle(pos, vel, elas, rad, mass);
-		
-		cradle->particles.push_back(p);
-
-		// Create a cluster of 4 particles for the moving balls
-		for (int x = 0; x < 4; x++)
-		{
-			vec2 vel(0.0f, 0.0f);
-			real elas = 1.0f;
-			real rad = 0.75f;
-			vec2 pos((x * rad * 2) - rad * 2, -2.0f);
-			if (x == 0)
-				pos.Set((-1 * rad * 2) - rad * 2 - 6.0f, 2.5f);
-			real mass = PI * rad * rad;
-			Particle* p = new Particle(pos, vel, elas, rad, mass);
-			
-			cradle->particles.push_back(p);
-		}
 
 		// Create a cluster for the anchor points of the balls
 		Cluster* anchors = new Cluster;
@@ -61,13 +36,54 @@ struct NewtonsCradleScene : public Scene
 			anchors->particles.push_back(p);
 		}
 
+		// Add a ball to hit the 4 balls
+		Cluster* cradle = new Cluster;
+
+		for (int x = 0; x < 5; x++)
+		{
+			vec2 vel(0.0f, 0.0f);
+			real elas = 1.0f;
+			real rad = 0.75f;
+			vec2 pos(anchors->particles[x]->pos_in_meters.x, -2.0f);
+			real mass = PI * rad * rad;
+			if (x == 0)
+			{
+				real len = 6.0f;
+				real radian = PI;
+				radian = -3;
+				real posx = cos(radian) * len;
+				real posy = sin(radian) * len;
+				posx += anchors->particles[x]->pos_in_meters.x;
+				posy += 4.0f;
+
+				pos.Set(posx, posy);
+			}
+			if (x == 1)
+			{
+				real len = 6.0f;
+				real radian = (7.0f*PI) / 6.0f;
+				radian = -2.5f;
+				real xpos = cos(radian) * len;
+				real ypos = sin(radian) * len;
+				xpos += anchors->particles[x]->pos_in_meters.x;
+				ypos += 4.0f;
+
+				pos.Set(xpos, ypos);
+			}
+			Particle* p = new Particle(pos, vel, elas, rad, mass);
+			
+			cradle->particles.push_back(p);
+		}
+
+
+
 		// Add springs connecting the balls to their anchor points
 		for (int n = 0; n < 5; n++)
 		{
 			Particle* pA = cradle->particles[n];
 			Particle* pB = anchors->particles[n];
 			real len = 6.0f;
-			real stiffness = 2000000.0f;
+			real stiffness = 20000.0f;
 			Spring* s = new Spring(pA, pB, len, stiffness);
 
 			anchors->springs.push_back(s);
