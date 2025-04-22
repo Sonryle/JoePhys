6996b78700857d55a8f141c7ab328bc1c2bac663
Joseph <cathcartj08@gmail.com>
2025-04-22 16:11:48 +0200

    Added New Scene Header files

---
+++ testbed/include/scenes/AirResistanceScene.hpp
@@ -0,0 +1,108 @@
+#ifndef NEWTONS_CRADLE_SCENE
+#define NEWTONS_CRADLE_SCENE
+
+// TEMPORARY FOR "stderr"
+#include <cstdio>
+
+#include "../Scenes.hpp"
+#include "../Settings.hpp"
+#include "../Renderer.hpp"
+#include "JoePhys/Particle.hpp"
+#include "JoePhys/Spring.hpp"
+
+struct NewtonsCradleScene : public Scene
+{
+	// constructors & destructors
+	void SetUpScene() override
+	{
+		SetUpSceneColours();
+		world->Create(settings.simulation_hertz, settings.sub_steps, settings.gravity);
+		double PI = 3.141592653589;
+
+		// Create a cluster for the anchor points of the balls
+		Cluster* anchors = new Cluster;
+		for (int x = 0; x < 5; x++)
+		{
+			vec2 vel(0.0f, 0.0f);
+			real elas = 1.0f;
+			real rad = 0.25f;
+			vec2 pos((x * 1.5f) - 3.0f, 4.0f);
+			real mass = PI * rad * rad;
+			Particle* p = new Particle(pos, vel, elas, rad, mass, 1);
+			
+			anchors->particles.push_back(p);
+		}
+
+		// Add a ball to hit the 4 balls
+		Cluster* cradle = new Cluster;
+
+		for (int x = 0; x < 5; x++)
+		{
+			vec2 vel(0.0f, 0.0f);
+			real elas = 1.0f;
+			real rad = 0.75f;
+			vec2 pos(anchors->particles[x]->pos.x, -2.0f);
+			real mass = PI * rad * rad;
+			if (x == 0)
+			{
+				real len = 6.0f;
+				real radian = PI;
+				real posx = cos(radian) * len;
+				real posy = sin(radian) * len;
+				posx += anchors->particles[x]->pos.x;
+				posy += 4.0f;
+
+				pos.Set(posx, posy);
+			}
+			Particle* p = new Particle(pos, vel, elas, rad, mass, 0);
+			
+			cradle->particles.push_back(p);
+		}
+
+
+
+		// Add springs connecting the balls to their anchor points
+		for (int n = 0; n < 5; n++)
+		{
+			Particle* pA = cradle->particles[n];
+			Particle* pB = anchors->particles[n];
+			real len = 6.0f;
+			real stiffness = 10000.0f;
+			Spring* s = new Spring(pA, pB, len, stiffness);
+
+			anchors->springs.push_back(s);
+		}
+		
+		// Add clusters to world
+		world->clusters.push_back(cradle);
+		world->clusters.push_back(anchors);
+	}
+
+	void SetUpSceneColours() override
+	{
+		colours.background = Palette::JP_DARK_PURPLE;
+		colours.spring = Palette::JP_DARK_GRAY;
+		colours.particle = Palette::JP_GREEN;
+		colours.particle_outline = Palette::JP_DARK_GRAY;
+		colours.static_particle = Palette::JP_GRAY;
+		colours.static_particle_outline = Palette::JP_DARK_GRAY;
+	}
+
+	void SetUpSceneSettings() override
+	{
+		settings.Reset();
+
+		settings.attraction_tool_strength = 25.0f;
+		settings.repulsion_tool_strength = 25.0f;
+
+		settings.gravity.Set(0.0f, -9.8f);
+		settings.circle_res = 20;
+		settings.chunk_scale = 1.7f;
+		settings.sub_steps = 64;
+
+		camera.center.Set(0.0f, 0.0f);
+		camera.zoom = 2.0f;
+	}
+};
+
+#endif
---
+++ testbed/include/scenes/WeightsScene.hpp
@@ -0,0 +1,93 @@
+#ifndef JP_ROPE_SCENE
+#define JP_ROPE_SCENE
+
+#include "../Scenes.hpp"
+#include "../Settings.hpp"
+#include "../Renderer.hpp"
+#include "JoePhys/Particle.hpp"
+
+#include <cstdio>	// for "stderr" file path constant
+
+struct RopeScene : public Scene
+{
+	// constructors & destructors
+	void SetUpScene() override
+	{
+		SetUpSceneColours();
+		world->Create(settings.simulation_hertz, settings.sub_steps, settings.gravity);
+		double PI = 3.141592653589;
+
+		// Create a cluster for the particles to live in
+		Cluster* obstacles = new Cluster;
+
+		// Add two particles to the cluster
+		vec2 posA(-2.66f, -0);
+		vec2 velA(0.0f, 0.0f);
+		real elasticityA = 0.2f;
+		real radiusA = 1.5f;
+		real massA = PI * radiusA * radiusA;
+		Particle* partA = new Particle(posA, velA, elasticityA, radiusA, massA, 1);
+		obstacles->particles.push_back(partA);
+
+		vec2 posB(3, 0);
+		vec2 velB(-0.0f, -0.0f);
+		real elasticityB = 0.2f;
+		real radiusB = 1;
+		real massB = PI * radiusB * radiusB;
+		Particle* partB = new Particle(posB, velB, elasticityB, radiusB, massB, 1);
+		obstacles->particles.push_back(partB);
+
+		// Create a rope
+		Cluster* rope = new Cluster;
+		Particle* old_part = nullptr;
+		for (int x = -28; x < 28; x++)
+		{
+			vec2 vel(0.0f, 0.0f);
+			real radius = 0.1f;
+			vec2 pos(radius * 3.0f * x, 2.5f);
+			real elasticity = 0.2f;
+			real mass = PI * radius * radius;
+			Particle* myParticle = new Particle(pos, vel, elasticity, radius, mass, 0);
+			// Add the particle to the physics object
+			rope->particles.push_back(myParticle);
+
+			real stiffness = 2500.0f;
+			if (old_part != nullptr)
+			{
+				real len = length(old_part->pos - myParticle->pos);
+				Spring* s = new Spring(old_part, myParticle, len, stiffness);
+				rope->springs.push_back(s);
+			}
+			old_part = myParticle;
+		}
+
+		// Add cluster to the world
+		world->clusters.push_back(obstacles);
+		world->clusters.push_back(rope);
+	}
+
+	void SetUpSceneColours() override
+	{
+		colours.background = Palette::JP_PURPLE;
+		colours.spring = Palette::JP_DARK_GRAY;
+		colours.particle = Palette::JP_GREEN;
+		colours.particle_outline = Palette::JP_DARK_GRAY;
+		colours.static_particle = Palette::JP_DARK_GREEN;
+		colours.static_particle_outline = Palette::JP_DARK_GRAY;
+	}
+
+	void SetUpSceneSettings() override
+	{
+		settings.Reset();
+
+		settings.gravity.Set(0.0f, -9.8f);
+		settings.circle_res = 25;
+		settings.chunk_scale = 1.0f;
+		settings.sub_steps = 40;
+
+		camera.center.Set(0.0f, 0.0f);
+		camera.zoom = 1.0f;
+	}
+};
+
+#endif
