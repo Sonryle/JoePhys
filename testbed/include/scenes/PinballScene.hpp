8a2e18c420793dadafe66ec830f655a640841c5e
Joseph <cathcartj08@gmail.com>
2025-04-23 14:43:25 +0200

    Added red & blue balls to PinBall scene

--- testbed/include/scenes/PinballScene.hpp
+++ testbed/include/scenes/PinballScene.hpp
@@ -18,7 +18,7 @@ struct PinballScene : public Scene
 		// Create a cluster for the grid of particles (sand) to go in
 		Cluster* sand = new Cluster;
 
-		// Create an 11x11 grid of particles to go in our cluster
+		// Create an 11x9 grid of particles to go in our cluster
 		for (int y = -5; y < 6; y++)
 			for (int x = -4; x < 5; x++)
 			{
@@ -126,6 +126,77 @@ struct PinballScene : public Scene
 
 		camera.center.Set( 0.0f, 3.5f);
 		camera.zoom = 1.4f;
+	}
+
+	void Render() override
+	{
+		// loop over every cluster in the world
+		for (int c = 0; c < world->clusters.size(); c++)
+		{
+			// loop over every particle in each cluster
+			for (Particle* p : world->clusters[c]->particles)
+			{
+				// Render each particle
+				int segments = settings.circle_res;
+				real rad = p->radius;
+				vec2 pos = p->pos;
+				colour col;
+				colour outline_col;
+				if (p->is_static)
+				{
+					col = palette.colours[colours.static_particle];
+					outline_col = palette.colours[colours.static_particle_outline];
+				}
+				else
+				{
+					col = palette.colours[colours.particle];
+					outline_col = palette.colours[colours.particle_outline];
+				}
+				if (p == world->clusters[1]->particles[49])
+					col = palette.colours[Palette::JP_DARK_GREEN];
+				if (p == world->clusters[1]->particles[53])
+					col = palette.colours[Palette::JP_RED];
+	
+				// Render particle
+				renderer.AddSolidCircle(pos, rad, segments, col, outline_col);
+			}
+			// loop over every spring in each cluster
+			if (settings.enable_springs)
+				for (Spring* s : world->clusters[c]->springs)
+				{
+					// Render each spring
+					vec2 posA = s->particleA->pos;
+					vec2 posB = s->particleB->pos;
+					colour col = palette.colours[colours.spring];
+					renderer.AddLine(posA, posB, col);
+				}
+		}
+		
+		// Render a circle around selected particle
+		colour outline_col(1.0f, 1.0f, 1.0f, 1.0f);
+		if (selected_particle != nullptr)
+			renderer.AddCircle(selected_particle->pos, selected_particle->radius * 1.2f, settings.circle_res, outline_col);
+		
+		// Render chunks
+		if (settings.render_chunks)
+			for (auto& [key, particles] : world->chunks)
+			{
+				real x, y;
+				world->HashKeyToChunkCoords(key, &x, &y);
+		
+				real left = x;
+				real right = x + settings.chunk_scale;
+				real top = y + settings.chunk_scale;
+				real bottom = y;
+				colour col(1.0f, 1.0f, 0.0f, 0.25f);
+				colour out_col(0.3f, 0.3f, 0.0f, 1.00f);
+				renderer.AddTriangle(vec2(left, top), vec2(right, top), vec2(left, bottom), col);
+				renderer.AddTriangle(vec2(right, top), vec2(left, bottom), vec2(right, bottom), col);
+				renderer.AddLine(vec2(left, top), vec2(right, top), out_col);
+				renderer.AddLine(vec2(left, top), vec2(left, bottom), out_col);
+				renderer.AddLine(vec2(right, bottom), vec2(right, top), out_col);
+				renderer.AddLine(vec2(right, bottom), vec2(left, bottom), out_col);
+			}
 	}
 };
 
